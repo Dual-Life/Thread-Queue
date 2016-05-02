ade7a51decc404d5fa38cf04af12db646f08a83f
Jerry D. Hedden <jdhedden@cpan.org>
2016-05-02 10:20:43 -0400

    MetaCPAN and code repository links in POD

--- lib/Thread/Queue.pm
+++ lib/Thread/Queue.pm
@@ -618,11 +618,11 @@ Passing array/hash refs that contain objects may not work for Perl prior to
 
 =head1 SEE ALSO
 
+Thread::Queue on MetaCPAN:
+L<https://metacpan.org/release/Thread-Queue>
+
 Code repository:
 L<https://github.com/Dual-Life/Thread-Queue>
-
-Thread::Queue Discussion Forum on CPAN:
-L<http://www.cpanforum.com/dist/Thread-Queue>
 
 L<threads>, L<threads::shared>
 
